bd85b63b95590f14752120b91e023e30673a8a24
root <9548holbertonstudents.com>
2024-08-08 13:23:32 +0200

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@
  *
 */
 
+/*
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
@@ -35,3 +36,41 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
+*/
+
+#include <stddef.h>
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm.
+ *
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int temp;
+    size_t i, j, k;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                /* Échanger array[j] et array[j + 1] */
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                /* Imprimer le tableau après chaque permutation */
+                for (k = 0; k < size; k++)
+                {
+                    printf("%d ", array[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
+
