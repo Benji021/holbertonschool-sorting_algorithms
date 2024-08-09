9903154a9bf1fb8bd5edf0325a6cdc39cfc64dbd
root <9548holbertonstudents.com>
2024-08-09 09:24:00 +0200

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,4 +11,60 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	if (!list || !*list || !(*list)->next)
+	{
+		return;
+	}
 
+	listint_t *sorted = NULL;
+	listint_t *current = *list;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+	next = current->next;
+	listint_t *insert_pos = sorted;
+
+	if (!sorted || sorted->n >= current->n)
+	{
+		current->next = sorted;
+		current->prev = NULL;
+
+	    if (sorted)
+		{
+		sorted->prev = current;
+		}
+		sorted = current;
+	}
+	else
+	{
+	while (insert_pos->next && insert_pos->next->n < current->n)
+	{
+		insert_pos = insert_pos->next;
+	}
+		current->next = insert_pos->next;
+	if (insert_pos->next)
+		{
+		insert_pos->next->prev = current;
+		}
+
+	insert_pos->next = current;
+	current->prev = insert_pos;
+	
+// Affichage de l'état actuel de la liste après chaque insertion
+        listint_t *tmp = sorted;
+        printf("Current list: ");
+        while (tmp)
+        {
+            printf("%d ", tmp->n);
+            tmp = tmp->next;
+        }
+        printf("\n");
+	
+	}
+
+	current = next;
+	}
+
+	*list = sorted;
+}
