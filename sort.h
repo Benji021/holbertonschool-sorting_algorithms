8e4b50835d41f9bd2fcd17153ee4fed2c661f910
root <9548holbertonstudents.com>
2024-08-09 10:23:22 +0200

    quick_sort

--- sort.h
+++ sort.h
@@ -22,10 +22,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-int main(void);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
-int main(void);
 void selection_sort(int *array, size_t size);
-int main(void);
+void quick_sort(int *array, size_t size);
+
 #endif
